fd540bb02382222d71e5e5abcc38bb1bb6c52168
Austin Ellsworth <austin_ellsworth1@yahoo.com>
2020-06-08 17:28:47 -0400

    Added product page and mockAPI

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ function Header(){
     return(
         <header className="border-b p-3 flex justify-between items-center">
             <span className="font-bold">
-                AppName
+                AustinsMarket
             </span>
 
             <Navigation />

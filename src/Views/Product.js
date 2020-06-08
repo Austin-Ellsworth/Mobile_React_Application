08d9f24925370aadbdc8626d6a932b76483551d2
Austin Ellsworth <austin_ellsworth1@yahoo.com>
2020-06-08 17:44:12 -0400

    Added loader while page loading

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,37 +1,52 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import Loader from '../Components/Loader'
 
 function Product(){
     const { id } = useParams()
     const url = `https://5edea222e36dd000166c857d.mockapi.io/products/${id}`
-    const [product, setProduct] = useState(null)
+    const [product, setProduct] = useState({
+        loading: false,
+        data: null
+    })
 
     let content = null
 
     useEffect(() => {
+        setProduct({
+            loading: true,
+            data: null
+        })
         axios.get(url).then(response => {
-            setProduct(response.data)
+            setProduct({
+                loading: false,
+                data: response.data
+            })
         })
     }, [url])
 
-    if(product){
+    if(product.loading){
+        content = <p><Loader></Loader></p>
+    }
+
+    if(product.data){
         content = 
             <div>
                 <h1 className="text-2xl font-bold mb-3">
-                    {product.name}
+                    {product.data.name}
                 </h1>
                 <div>
                     <img
-                        src={product.images[0].images}
-                        alt={product.name}
+                        src={product.data.images[0].images}
+                        alt={product.data.name}
                     />
                 </div>
                 <div className="font-bold text-xl mb-3">
-                    $ {product.price}
+                    $ {product.data.price}
                 </div>
                 <div>
-                    {product.description}
+                    {product.data.description}
                 </div>
             </div>
     }
